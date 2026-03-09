cafefc246e355e38d4bb9f95c6329d6b3c5a611a
loginkarun <loginkarun@gmail.com>
2026-03-09 20:32:12 +0530

    Auto commit: Scripts committed on 2026-03-09-15-02-09

---
+++ code/src/main/java/com/myproject/models/dtos/CartItemUpdateRequest.java
@@ -0,0 +1,19 @@
+package com.myproject.models.dtos;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItemUpdateRequest {
+    
+    @NotNull(message = "Quantity cannot be null")
+    @Min(value = 1, message = "Quantity must be at least 1")
+    @Max(value = 100, message = "Quantity cannot exceed 100")
+    private Integer quantity;
+}
